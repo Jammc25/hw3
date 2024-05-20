6585c31a4c66bb85302413eaf736a8427b21fdae
James McManus <james.mcmanus@kellogg.northwestern.edu>
2024-05-20 03:35:38 +0000

    Additional progress for entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,25 @@
 class EntriesController < ApplicationController
+  def index
+    @entries = Entry.all
+  end
+  
+  def show
+    @entry = Entry.find_by({"id" => params["id"]})
+    @place = Place.find_by({"id" =>@entry["place_id"]})
+  end
+  
+  def new
+    @place = Place.find_by({"id" =>@entry["place_id"]})
+  end
+  
+  def create
+    @entry = Entry.new
+    @entry.title = params["title"]
+    @entry.description = params["description"]
+    @entry.posted_on = params["posted_on"]
+    @entry["place_id"] = params["place_id"]
+    @entry.save
+  end
+  redirect_to "/places/#{@entry["place_id"]}"
+
 end
